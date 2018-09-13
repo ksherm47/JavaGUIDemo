dafef7e721cb38532a290ea01d17d26abd6c4d81
Kenneth Sherman <kisp7c@mst.edu>
2018-09-13 13:25:25 -0500

    Added phase configuration pane

--- TeamMark/src/sample/UIController.java
+++ TeamMark/src/sample/UIController.java
@@ -1,7 +1,26 @@
 package sample;
 
-public class UIController {
-    public void openPhase(int x) {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class UIController {
+
+    //@FXML Button phase1_button;
+    public void openPhase() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("phase_pane.fxml"));
+            Stage phaseStage = new Stage();
+            phaseStage.setTitle("Phase Configuration");
+            phaseStage.setScene(new Scene(root, 550, 400));
+            phaseStage.show();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
