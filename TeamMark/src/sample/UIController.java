11e4f6db5fc5142623b25a7bbae798ab7d55d004
lukaivicevic <li4g5@mst.edu>
2018-09-18 13:28:51 -0500

    Pass phase number through to phase config window and set initial focus to username login field

--- TeamMark/src/sample/UIController.java
+++ TeamMark/src/sample/UIController.java
@@ -1,19 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class UIController {
 
     //@FXML Button phase1_button;
-    public void openPhase() {
+    @FXML
+    public void openPhase(ActionEvent event) {
         try {
+            String buttonId = ((Button)event.getSource()).getId();
+            String phaseNumber = buttonId.substring(5,6);
             Parent root = FXMLLoader.load(getClass().getResource("phase_pane.fxml"));
             Stage phaseStage = new Stage();
-            phaseStage.setTitle("Phase Configuration");
+            phaseStage.setTitle("Phase Configuration - " + phaseNumber);
             phaseStage.setScene(new Scene(root, 640, 480));
             phaseStage.setResizable(false);
             phaseStage.show();
