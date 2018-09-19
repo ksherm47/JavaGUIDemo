cd80ececeac8001293c06a6c316e410b7a718222
Jimmy Lin <jimmylin19950412@gmail.com>
2018-09-19 14:38:21 -0500

    Added the ability for buttons to change colors and changed button layout. green = on. default = off.

--- TeamMark/src/sample/UIController.java
+++ TeamMark/src/sample/UIController.java
@@ -1,15 +1,9 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import javafx.event.Event;
 import javafx.scene.control.Control;
@@ -17,21 +11,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class UIController {
-<<<<<<< HEAD
     @FXML Button phase1_button;
     public void openPhase() {
-=======
-
-    //@FXML Button phase1_button;
-    @FXML
-    public void openPhase(ActionEvent event) {
->>>>>>> 11e4f6db5fc5142623b25a7bbae798ab7d55d004
         try {
-            String buttonId = ((Button)event.getSource()).getId();
-            String phaseNumber = buttonId.substring(5,6);
             Parent root = FXMLLoader.load(getClass().getResource("phase_pane.fxml"));
             Stage phaseStage = new Stage();
-            phaseStage.setTitle("Phase Configuration - " + phaseNumber);
+            phaseStage.setTitle("Phase Configuration");
             phaseStage.setScene(new Scene(root, 640, 480));
             phaseStage.setResizable(false);
             phaseStage.show();
