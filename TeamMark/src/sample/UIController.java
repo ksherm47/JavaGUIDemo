a1d88801d3a381e334019ef7a559a21baafd1c3d
Kenneth Sherman <kensherm@r02kisp7c.device.mst.edu>
2018-09-20 14:01:32 -0500

    removed stuff we actually didn't need

--- TeamMark/src/sample/UIController.java
+++ TeamMark/src/sample/UIController.java
@@ -25,16 +25,4 @@ public class UIController {
         }
     }
 
-
-    //changes color of button on press
-    public void colorChange(Event evt) {
-        //changes color of button to green if not already green
-        if(((Control)evt.getSource()).getStyle() != "-fx-base: #b6e7c9;") {
-            ((Control) evt.getSource()).setStyle("-fx-base: #b6e7c9;");
-        }
-        //restores default color of button
-        else {
-            ((Control) evt.getSource()).setStyle("");
-        }
-    }
 }
